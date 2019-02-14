dde7e72f121087528afc3823f13be1cc3b9756ce
eduardo-magana <42873295+eduardo-magana@users.noreply.github.com>
2019-02-14 11:48:28 -0600

    Update Challenge-Mosaico.js

--- Challenge-Mosaico.js
+++ Challenge-Mosaico.js
@@ -1,4 +1,3 @@
-
 function setup() {
 	createCanvas(500, 500);
 }
@@ -7,7 +6,7 @@ function draw() {
 	background(255);
 	for(var y = 5; y < 500; y = y + 10){
 	for(var x = 5; x < 500; x = x + 10){
-		fill(255,0,0);
+		fill(random(0,255),random(0,255),random(0,255));
 		ellipse( x, y,10,10);
 	}
 	}
